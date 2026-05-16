90729be2e2b7aa5ba6677d55575d853f1859b83d
Marc Velasquez <104043486+marccodesstuff@users.noreply.github.com>
2026-05-17 07:36:34 +0800

    Add ThemeProvider for theme context

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './tactile-init' // Import Three.js + GSAP initialization
 import App from './App.tsx'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
